5825ee81d4e1bed281acd98ec7bcd0e46b390e8b
Kashiwa <13825170+ksw2000@users.noreply.github.com>
2025-05-11 16:59:46 +0800

    feat: leader board score bar

--- web/pcb-badge-leaderboard/src/app/leaderboard/leaderboard.component.ts
+++ web/pcb-badge-leaderboard/src/app/leaderboard/leaderboard.component.ts
@@ -1,5 +1,5 @@
 import { DecimalPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-leaderboard',
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css'
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements AfterViewInit {
+  @ViewChild('team1Block') team1Block!: ElementRef;
+  @ViewChild('team2Block') team2Block!: ElementRef;
+  team1Score: number = 2000
+  team2Score: number = 8000
   items: leaderboard[] = []
   constructor(){
     for(let i = 0; i < 1000; i++){
@@ -16,6 +20,21 @@ export class LeaderboardComponent {
         score: (1000-i)*10,
       })
     }
+  }
+  
+  ngAfterViewInit(): void {
+    this.updateBlock();
+  }
+
+  computeTeam1Percent(): number {
+    return this.team1Score / (this.team1Score + this.team2Score)
+  }
+
+  updateBlock() {
+    let p1 = Math.round(this.computeTeam1Percent() * 100);
+    let p2 = 100 - p1;
+    this.team1Block.nativeElement.style.width = `${p1}%`;
+    this.team2Block.nativeElement.style.width = `${p2}%`;
   }
 }
 
