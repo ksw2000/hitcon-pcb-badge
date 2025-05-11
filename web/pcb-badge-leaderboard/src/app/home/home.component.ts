bf1d1324ed13e07c9f5792a0aa6e8f6a0775a941
Kashiwa <13825170+ksw2000@users.noreply.github.com>
2025-05-11 21:53:49 +0800

    feat: floor selection button

--- web/pcb-badge-leaderboard/src/app/home/home.component.ts
+++ web/pcb-badge-leaderboard/src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  floor: number = 0;
+  floorImg: string[] = ['./2f3f.svg', '4f.svg'];
+  changeFloor(floor: number) {
+    this.floor = floor;
+  }
 }
