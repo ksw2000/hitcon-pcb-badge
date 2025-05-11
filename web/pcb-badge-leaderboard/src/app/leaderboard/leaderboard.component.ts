3f89afa0edc9b975a912bc2d68662189c0a53c65
Kashiwa <13825170+ksw2000@users.noreply.github.com>
2025-05-11 22:19:20 +0800

    feat: color the different team

--- web/pcb-badge-leaderboard/src/app/leaderboard/leaderboard.component.ts
+++ web/pcb-badge-leaderboard/src/app/leaderboard/leaderboard.component.ts
@@ -1,23 +1,24 @@
-import { DecimalPipe } from '@angular/common';
+import { CommonModule, DecimalPipe } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, CommonModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css'
 })
 export class LeaderboardComponent implements AfterViewInit {
+  @ViewChild('team0Block') team0Block!: ElementRef;
   @ViewChild('team1Block') team1Block!: ElementRef;
-  @ViewChild('team2Block') team2Block!: ElementRef;
-  team1Score: number = 2000
-  team2Score: number = 8000
+  team0Score: number = 2000
+  team1Score: number = 8000
   items: leaderboard[] = []
   constructor(){
-    for(let i = 0; i < 1000; i++){
+    for(let i = 0; i < 20; i++){
       this.items.push({
         name: "name name name name",
         score: (1000-i)*10,
+        teamID: Math.floor(Math.random() * 2),
       })
     }
   }
@@ -26,19 +27,22 @@ export class LeaderboardComponent implements AfterViewInit {
     this.updateBlock();
   }
 
-  computeTeam1Percent(): number {
-    return this.team1Score / (this.team1Score + this.team2Score)
+  computeTeam0Percent(): number {
+    return this.team0Score / (this.team0Score + this.team1Score)
   }
 
   updateBlock() {
-    let p1 = Math.round(this.computeTeam1Percent() * 100);
+    let p1 = Math.round(this.computeTeam0Percent() * 100);
     let p2 = 100 - p1;
-    this.team1Block.nativeElement.style.width = `${p1}%`;
-    this.team2Block.nativeElement.style.width = `${p2}%`;
+    this.team0Block.nativeElement.style.width = `${p1}%`;
+    this.team1Block.nativeElement.style.width = `${p2}%`;
   }
 }
 
 interface leaderboard {
   name: string
   score: number
+  teamID: number
 }
+
+// TODO: use service to get and keep data
