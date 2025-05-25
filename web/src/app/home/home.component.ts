fa842a467efd0fd3a2bcf918f55ec8c851a4d7e8
Kashiwa <13825170+ksw2000@users.noreply.github.com>
2025-05-25 20:15:29 +0800

    feat: store the state of floor and score position

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StateService } from '../state.service';
 
 @Component({
   selector: 'app-home',
@@ -11,41 +12,61 @@ export class HomeComponent {
   @ViewChild('mapWrapper') mapWrapper?: ElementRef;
   @ViewChild('mapScaler') mapScaler?: ElementRef;
 
-  scaler = 99;
   readonly scalerMax = 299;
   readonly scalerMin = 79;
+  floor = 0;
+  scaler = 99;
+  readonly floorImg: string[] = ['./2f3f.svg', './4f.svg'];
+
+  constructor(public state: StateService) { }
 
   ngAfterViewInit(): void {
-    this.centerWrapper();
+    this.zoom(0);
+    console.log("ngAfterViewInit()", this.state.scrollLeft, this.state.scrollTop);
+    this.moveWrapper(this.state.scrollLeft, this.state.scrollTop);
   }
 
-  floor: number = 0;
-  floorImg: string[] = ['./2f3f.svg', '4f.svg'];
+  ngOnInit() {
+    this.floor = this.state.floor;
+    this.scaler = this.state.scaler;
+  }
+
   changeFloor(floor: number) {
     this.floor = floor;
+    this.state.floor = this.floor;
   }
 
-  zoom(zoomIn: boolean) {
+  zoom(delta: number) {
     if (!this.mapScaler) {
       return;
     }
 
-    if (zoomIn) {
-      this.scaler = Math.min(this.scaler + 10, this.scalerMax);
-    } else {
-      this.scaler = Math.max(this.scaler - 10, this.scalerMin);
-    }
+    this.scaler = Math.min(this.scaler + delta, this.scalerMax);
+    this.state.scaler = this.scaler;
 
     const el = this.mapScaler.nativeElement;
     el.style.height = `${this.scaler}%`;
   }
 
-  centerWrapper() {
+  moveWrapper(left: number | null, top: number | null) {
     if (!this.mapWrapper) {
       return;
     }
     const el = this.mapWrapper.nativeElement;
-    el.scrollLeft = Math.max(0, (el.scrollWidth - el.offsetWidth) / 2);
-    el.scrollTop = Math.max(0, (el.scrollHeight - el.offsetHeight) / 2);
+    if (left != null && top != null) {
+      el.scrollLeft = Math.max(0, left);
+      el.scrollTop = Math.max(0, top);
+    } else {
+      el.scrollLeft = Math.max(0, (el.scrollWidth - el.offsetWidth) / 2);
+      el.scrollTop = Math.max(0, (el.scrollHeight - el.offsetHeight) / 2);
+    }
+  }
+
+  onMapWrapperScroll(event: Event) {
+    const target = event.target as HTMLElement;
+    if (this.mapWrapper && this.mapWrapper.nativeElement === target) {
+      this.state.scrollLeft = target.scrollLeft;
+      this.state.scrollTop = target.scrollTop;
+    }
   }
 }
