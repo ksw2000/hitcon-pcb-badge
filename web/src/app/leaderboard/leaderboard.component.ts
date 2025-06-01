d0f94b5e30340059edee2e5f2c8902c640be95b0
Kashiwa <13825170+ksw2000@users.noreply.github.com>
2025-06-01 19:55:54 +0800

    refact: the width of team score bar

--- web/src/app/leaderboard/leaderboard.component.ts
+++ web/src/app/leaderboard/leaderboard.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, DecimalPipe } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-leaderboard',
@@ -7,35 +7,26 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css'
 })
-export class LeaderboardComponent implements AfterViewInit {
-  @ViewChild('team0Block') team0Block!: ElementRef;
-  @ViewChild('team1Block') team1Block!: ElementRef;
+export class LeaderboardComponent {
   team0Score: number = 2000
   team1Score: number = 8000
   items: leaderboard[] = []
-  constructor(){
-    for(let i = 0; i < 20; i++){
+  constructor() {
+    for (let i = 0; i < 20; i++) {
       this.items.push({
         name: "name name name name",
-        score: (1000-i)*10,
+        score: (1000 - i) * 10,
         teamID: Math.floor(Math.random() * 2),
       })
     }
   }
-  
-  ngAfterViewInit(): void {
-    this.updateBlock();
+
+  team0Percent(): number {
+    return (this.team0Score / (this.team0Score + this.team1Score)) * 100
   }
 
-  computeTeam0Percent(): number {
-    return this.team0Score / (this.team0Score + this.team1Score)
-  }
-
-  updateBlock() {
-    let p1 = Math.round(this.computeTeam0Percent() * 100);
-    let p2 = 100 - p1;
-    this.team0Block.nativeElement.style.width = `${p1}%`;
-    this.team1Block.nativeElement.style.width = `${p2}%`;
+  team1Percent(): number {
+    return (1 - this.team0Score / (this.team0Score + this.team1Score)) * 100
   }
 }
 
