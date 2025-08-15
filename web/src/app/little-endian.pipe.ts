bdff5202dffed4c4bb419bdf1694f0d858f28b73
Kashiwa <13825170+ksw2000@users.noreply.github.com>
2025-08-15 12:27:28 +0800

    fix little-endian bug

--- web/src/app/little-endian.pipe.ts
+++ web/src/app/little-endian.pipe.ts
@@ -7,13 +7,10 @@ export class LittleEndianPipe implements PipeTransform {
   transform(value: number): string {
     const tokens: string[] = [];
     const mask = 0xff;
-    while (value > 0) {
+    while (tokens.length < 4) {
       const token = value & mask;
       tokens.push(token.toString(16).padStart(2, "0"));
       value = value >> 8;
-    }
-    while(tokens.length < 4) {
-      tokens.push("00");
     }
     return tokens.join(" ");
   }
